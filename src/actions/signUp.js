7a533398812a04a0ff1ed2bb11c84745d55cfc4e
JC Harrington <jc@brandibble.co>
2021-12-26 13:54:16 -0600

    Update signUpCustomer callback to pass response as argument

--- src/actions/signUp.js
+++ src/actions/signUp.js
@@ -5,20 +5,18 @@ import { loginCustomer } from './customer/account'
 
 export const resetSignUp = () => ({ type: RESET_SIGN_UP })
 
-export const signUpCustomer = (data, callback) => async (
-  dispatch,
-  getState
-) => {
-  const { api } = getState().config
-  if (!api) return
-  dispatch(pending(SIGN_UP))
-  try {
-    await api.postSignUp(data)
-    dispatch(loginCustomer(data.email, data.password))
-    dispatch(fulfill(SIGN_UP))
-    if (callback) callback()
-  } catch (err) {
-    const errors = makeFormErrors(err)
-    dispatch(reject(SIGN_UP, errors))
+export const signUpCustomer =
+  (data, callback) => async (dispatch, getState) => {
+    const { api } = getState().config
+    if (!api) return
+    dispatch(pending(SIGN_UP))
+    try {
+      const response = await api.postSignUp(data)
+      dispatch(loginCustomer(data.email, data.password))
+      dispatch(fulfill(SIGN_UP))
+      if (callback) callback(response)
+    } catch (err) {
+      const errors = makeFormErrors(err)
+      dispatch(reject(SIGN_UP, errors))
+    }
   }
-}
