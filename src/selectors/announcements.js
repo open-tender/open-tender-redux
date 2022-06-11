9060d58685c4c40ecd8c37e6f7678a6114b9e852
JC Harrington <jc@brandibble.co>
2022-06-11 18:41:19 -0400

    Create hasAnnouncementPage selector

--- src/selectors/announcements.js
+++ src/selectors/announcements.js
@@ -5,3 +5,10 @@ export const selectAnnouncementsPage = page => state => {
   const { settings = null, entities = [] } = data || {}
   return { settings, entities, loading, error }
 }
+export const selectHasAnnouncementsPage = page => state => {
+  const { pages, loading } = state.data.announcements
+  const data = pages ? pages.find(i => i.page === page) : {}
+  const { entities = [] } = data || {}
+  const hasAnnouncements = entities && entities.length
+  return loading === 'pending' || hasAnnouncements ? true : false
+}
